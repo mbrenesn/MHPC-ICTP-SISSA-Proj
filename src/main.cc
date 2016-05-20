9edee663244848cde76818da9f60e5222bae491b
Marlon <mbrenes@ictp.it>
2016-05-20 19:10:53 +0200

    First commit on the serial version of the Krylov method, relies on uBlas

--- src/main.cc
+++ src/main.cc
@@ -8,9 +8,9 @@ int main()
 {
   unsigned int l = 4;
   unsigned int n = 2;
-  double V = 1.0;
+  double V = 0.2;
   double t = -1.0;
-  
+
   Basis basis(l,n);
 
   unsigned long long int *int_basis = new unsigned long long int[basis.basis_size()];
@@ -51,7 +51,24 @@ int main()
 
   sparse_hamiltonian.construct_hamiltonian_matrix(int_basis,V,t,l,n);
 
-  sparse_hamiltonian.print_hamiltonian();
+  //sparse_hamiltonian.print_hamiltonian();
+  
+  boost::numeric::ublas::vector<double> w(basis.basis_size(), 0.0);
+  boost::numeric::ublas::vector<double> v(basis.basis_size());
+
+  for(unsigned int i = 0; i < basis.basis_size(); ++i) v(i) = i + 1;
+
+  boost::numeric::ublas::matrix<double> mat(basis.basis_size(), basis.basis_size(), 0.0);
+  double tv = 1.0;
+  double tol = 1.0e-06;
+  unsigned int m = 6;
+  double err, hump;
+
+  sparse_hamiltonian.expv_krylov_solve(tv,w,err,hump,v,tol,m);
+  
+  std::cout << w << std::endl;
+  std::cout << err << std::endl;
+  std::cout << hump << std::endl;
 
   delete [] int_basis;
   delete [] bit_basis;
