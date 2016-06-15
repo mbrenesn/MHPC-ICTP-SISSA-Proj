bcf806276721a7a084522e56aebee28f571e2fd2
mbrenesn <mbrenes@ictp.it>
2016-06-15 18:26:39 +0200

    Some progress on time evolution

--- src/parallel-petsc-slepc/main.cc
+++ src/parallel-petsc-slepc/main.cc
@@ -18,6 +18,8 @@ int main(int argc, char **argv)
   unsigned long long int *int_basis = new unsigned long long int[basis.basis_size()];
   basis.construct_int_basis(int_basis);
 
+#if 0
+
   std::cout << "Here's the basis in int notation:" << std::endl;
   for(unsigned int i=0;i<basis.basis_size();++i) std::cout << int_basis[i] << std::endl;
 
@@ -45,6 +47,8 @@ int main(int argc, char **argv)
     std::cout << std::endl;
   }
 
+#endif
+
   // Sparse hamiltonian testing zone
   
   // Invoke the constructor with the basis size. The constructor will initialize PETSc
@@ -62,7 +66,6 @@ int main(int argc, char **argv)
   // Declare vectors and parameters
   Vec w;
   Vec v;
-  double tv = 1.0;
   double tol = 1.0e-06;
   int maxit = 30;
 
@@ -73,18 +76,39 @@ int main(int argc, char **argv)
  
   // An example of the RHS vector to test solutions. This will change into a prepared 
   // initial quantum state
-  PetscComplex z;
+  //PetscComplex z;
   PetscInt start, end;
   VecGetOwnershipRange(v, &start, &end);
 
-  for(int ii = start; ii < end; ++ii){
-    z = std::complex<double>(ii+1,ii+1); 
-    VecSetValues(v, 1, &ii, &z, INSERT_VALUES);
-  }
+  // An example of how to populate vectors
+  //for(int ii = start; ii < end; ++ii){
+  //  z = std::complex<double>(ii+1,0); 
+  //  VecSetValues(v, 1, &ii, &z, INSERT_VALUES);
+  //}
+
+  // Random population of the initial vector
+  PetscRandom rctx;
+  PetscRandomCreate(PETSC_COMM_WORLD, &rctx);
+  PetscRandomSetType(rctx, PETSCRAND);
+
+  PetscRandomSetInterval(rctx, 0.0, 1.0);
+  PetscRandomSeed(rctx);
+
+  VecSetRandom(v, rctx);
+
+  PetscRandomDestroy(&rctx);
 
   VecAssemblyBegin(v);  
   VecAssemblyEnd(v);
 
+  // Normalize the initial vector
+  PetscReal norm_initial;
+  VecNorm(v, NORM_2, &norm_initial);
+
+  VecNormalize(v, &norm_initial);
+
+  // Construct a time evolution vector in the same manner as the initial vector
+  // was constructed. Values are NOT copied over.
   VecDuplicate(v, &w);
 
   // We let PETSc decide the distribution among processes, let's pass this to the
@@ -103,15 +127,36 @@ int main(int argc, char **argv)
   delete [] int_basis;
 
   sparse_hamiltonian.print_hamiltonian();
-  
+ 
+  std::cout << "Initial state at t = 0" << std::endl;
   VecView(v, PETSC_VIEWER_STDOUT_WORLD);
 
   PetscLogDouble kryt1, kryt2;
 
+  /*** Time evolution ***/
   PetscTime(&kryt1);
-  sparse_hamiltonian.expv_krylov_solve(tv, tol, maxit, w, v);
-  PetscTime(&kryt2);
   
+  unsigned int iterations = 10;
+  double times[iterations] = {0.0,0.1,0.2,0.3,0.4,0.5,1.0,2.0,5.0,10.0};
+  PetscReal normcheck;
+  for(unsigned int tt = 1; tt < iterations; ++tt){
+   
+    sparse_hamiltonian.expv_krylov_solve(times[tt] - times[tt - 1], tol, maxit, w, v);
+    VecNorm(w, NORM_2, &normcheck);
+    if(normcheck > 1.001 || normcheck < 0.999){
+      std::cerr << "Normcheck failed!" << std::endl;
+      exit(1);
+    }
+
+    VecCopy(w, v);
+    std::cout << "Time" << "\t" << times[tt] << std::endl;
+    VecView(v, PETSC_VIEWER_STDOUT_WORLD);
+  }
+   
+  PetscTime(&kryt2);
+  /*** End time evolution ***/
+  
+  // Now the state at the last step is stored in v
   VecView(w, PETSC_VIEWER_STDOUT_WORLD);
 
   PetscReal norm;
@@ -122,14 +167,14 @@ int main(int argc, char **argv)
   if(sparse_hamiltonian.get_mpirank() == 0){  
     std::cout << "Size of the Hilbert space:" << std::endl;
     std::cout << basis.basis_size() << std::endl;
-    std::cout << "L2 norm of the w vec: " << norm << std::endl; 
+    std::cout << "L2 norm of the final state vec: " << norm << std::endl; 
     std::cout << "Time Construction: " << constt2 - constt1 << " seconds" << std::endl; 
-    std::cout << "Time Krylov: " << kryt2 - kryt1 << " seconds" << std::endl; 
+    std::cout << "Time Krylov Evolution: " << kryt2 - kryt1 << " seconds" << std::endl; 
     std::cout << "Time total: " << time2 - time1 << " seconds" << std::endl; 
   }
 
   VecDestroy(&v);  
   VecDestroy(&w);
-  delete [] bit_basis;
+  //delete [] bit_basis;
   return 0;
 }
