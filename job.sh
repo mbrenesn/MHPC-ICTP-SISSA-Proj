54aad53cbc91c589b335da1e2ec7b901d38f86e0
mbrenesn <mbrenes@ictp.it>
2016-07-27 14:53:28 +0200

    Example of job script

--- job.sh
+++ job.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #PBS -q regular
-#PBS -l nodes=2:ppn=20:mem160
-#PBS -l walltime=4:00:00
+#PBS -l nodes=16:ppn=20
+#PBS -l walltime=12:00:00
 #PBS -T flush_cache
 
 cd $PBS_O_WORKDIR
 
 module load openmpi/1.8.3/gnu/4.9.2
+module load mkl/11.1
 
-for i in 2 4 8 16 32 40
-do
-    echo NUM PROCS $i >> output_krylov;
-    mpirun -np $i ./parallel.x >> output_krylov
-done
+mpirun --map-by ppr:5:socket ./32_master.x -mfn_converged_reason -memory_view > 320p_32half_tmax100_5pprpersocket_master.dat
