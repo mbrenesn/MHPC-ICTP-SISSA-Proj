a2597957c1ac969e31a9ea60d4c5ee7a63e89f81
mbrenesn <mbrenes@ictp.it>
2016-08-26 18:50:44 +0200

    Job

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q regular
-#PBS -l nodes=16:ppn=20
-#PBS -l walltime=12:00:00
+#PBS -q wide
+#PBS -l nodes=32:ppn=20
+#PBS -l walltime=04:00:00
 #PBS -T flush_cache
 
 cd $PBS_O_WORKDIR
@@ -9,4 +9,4 @@ cd $PBS_O_WORKDIR
 module load openmpi/1.8.3/gnu/4.9.2
 module load mkl/11.1
 
-mpirun --map-by ppr:5:socket ./32_master.x -mfn_converged_reason -memory_view > 320p_32half_tmax100_5pprpersocket_master.dat
+mpirun --map-by ppr:10:socket ./32_comm_node.x -memory_view > 640p_32half_tmax100_10pprpersocket_comm_node.dat
